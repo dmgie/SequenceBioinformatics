143924d3d416eb94268e99db9d40e8de69d87801
Barbar <barbara.muehlbauer@student.uni-tuebingen.de>
2022-12-18 20:39:53 +0100

    added 2

--- assignment08/Minimap_YOUR_NAME.java
+++ assignment08/Minimap_YOUR_NAME.java
@@ -111,7 +111,21 @@ public class Minimap_YOUR_NAME {
 	public static int h(String s) {
 		var value = 0;
 		// todo: implement hashing as described in script
-		
+		int len = s.length();
+		for(int i = 0; i < len; i++ ) {
+			if(s.charAt(i) == 'A'){
+				value += 0;
+			}
+			else if(s.charAt(i) == 'T'){
+				value += Math.pow(4,len-i+1);
+			}
+			else if(s.charAt(i) == 'G'){
+				value += 2 * Math.pow(4,len-i+1);
+			}
+			else if(s.charAt(i) == 'C'){
+				value += 3 * Math.pow(4,len-i+1);
+			}
+		}
 		return value;
 	}
 
